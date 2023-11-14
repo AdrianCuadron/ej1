8abbb9eb168e92cafdcbf138b9ba6db45e9a6670
Adrian Cuadron <76923232+AdrianCuadron@users.noreply.github.com>
2023-11-14 16:42:35 +0100

    Update test.py: add test increment 1000

--- test.py
+++ test.py
@@ -14,3 +14,8 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_increment1000():
+    obj= Wallet(10)
+    obj.increment1000()
+    assert obj.get_balance() == 1100
